2e14b994503b5fcb18fcf0532cfe2c969d2b4b78
Ramasamy <64701795+RamasamySP@users.noreply.github.com>
2022-06-12 16:31:24 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from tkinter import*
+from tkinter import ttk
+from turtle import width
+from PIL import Image, ImageTk
+from tkinter import messagebox
+from trace import Trace
+from unicodedata import name
+from matplotlib.pyplot import text
+import mysql.connector
+import cv2
+import os
+import numpy as np
+
+class Train :
+    def __init__(self, root) :
+        self.root = root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("face Recognition System")
+
+        title_lbl = Label(self.root, text="Train Dataset", font=("times new roman", 35, "bold"), bg = "black", fg = "white")
+        title_lbl.place(x = 0, y = 0, width= 1530, height=45)
+
+        img_top = Image.open(r"images\grey.jpg")
+        img_top = img_top.resize((1530, 325), Image.ANTIALIAS)
+        self.photoimg_top = ImageTk.PhotoImage(img_top)
+
+        f_lab = Label(self.root, image=self.photoimg_top)
+        f_lab.place(x = 0, y = 55, width = 1530, height = 325)
+
+        #**************Button**********************
+        b_1 = Button(self.root, text = "Train Data", command=self.train_classifier, cursor="hand2", font=("times new roman",30, "bold" ), bg = "darkblue", fg = "white")
+        b_1.place(x = 0, y = 380, width = 1530, height = 60)
+
+
+        img_bottom = Image.open(r"images\grey.jpg")
+        img_bottom = img_bottom.resize((1530, 325), Image.ANTIALIAS)
+        self.photoimg_bottom = ImageTk.PhotoImage(img_bottom)
+
+        f_lab = Label(self.root, image=self.photoimg_bottom)
+        f_lab.place(x = 0, y = 440, width = 1530, height = 325)
+
+
+    def train_classifier(self) :
+        data_dir = ('data')
+        path = [os.path.join(data_dir, file) for file in os.listdir(data_dir)]
+
+        faces = []
+        ids = []
+
+        for image in path :
+            img = Image.open(image).convert('L')  #Gray scale image
+            imageNp=np.array(img, 'uint8')
+            id=int(os.path.split(image)[1].split('.')[1])
+
+            faces.append(imageNp)
+            ids.append(id)
+            cv2.imshow("Training", imageNp)
+            cv2.waitKey(1) == 13
+        ids = np.array(ids)
+
+        #************** Train the Classifier and Save ****************************
+        clf = cv2.face.LBPHFaceRecognizer_create()
+        clf.train(faces, ids)
+        clf.write("classifier.xml")
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Result", "Training Datasets Completed!")
+
+
+if __name__ == "__main__" :
+    root = Tk()
+    obj = Train(root)
+    root.mainloop()
